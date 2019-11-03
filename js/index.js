5cbe52e9dab25e3742de64d523e68cdd9194cc1a
Jared Brown <jared@Jareds-MBP.lan1>
2019-11-03 12:06:25 -0800

    added two events to first image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+const topImg = document.querySelector('.intro img');
+
+topImg.addEventListener("mouseenter", () => {
+    topImg.style.transform="scale(1.2)";
+    topImg.style.transition="all 0.3s";
+  })
+
+  topImg.addEventListener("mouseleave", () => {
+    topImg.style.transform = "scale(1)";
+    topImg.style.transition="all 0.3s";
+  })
